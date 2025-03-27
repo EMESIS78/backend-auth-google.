dc3c894f6e8cb2fca7d4b9f7a1dcef4758abe159
EMESIS78 <fabrismal351@gmail.com>
2025-03-27 01:25:01 -0500

    Fix: usar PORT en lugar de 3000

--- server.js
+++ server.js
@@ -76,4 +76,5 @@ app.get("/api/user", verifyToken, (req, res) => {
     res.json({ user: req.user });
 });
 
-app.listen(3000, () => console.log("✅ Servidor corriendo en http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Servidor corriendo en el puerto ${PORT}`));
